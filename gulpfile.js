d369695875781f39fc78b0dbe56dc234843ae3d2
Michael McDermott <mmcdermott@mad-computer-scientist.com>
2016-05-14 21:54:15 -0500

    Added error handling to gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -1,7 +1,8 @@
 var gulp = require('gulp'),
     jshint = require('gulp-jshint'),
     mocha = require('gulp-mocha'),
-    pandoc = require('gulp-pandoc');
+    pandoc = require('gulp-pandoc'),
+    plumber = require('gulp-plumber');
 
 gulp.task('generate documentation', function() {
     gulp.src('doc/specification*.md')
@@ -16,12 +17,24 @@ gulp.task('generate documentation', function() {
 
 gulp.task('lint server module', function() {
     gulp.src(['src/server/*.js', 'src/server/lib/*.js', 'src/server/spec/**/*.js'])
+        .pipe(plumber({
+            errorHandler: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        }))                                                                            
         .pipe(jshint())
         .pipe(jshint.reporter('default'));
 });
 
 gulp.task('test', function() {
     gulp.src('src/server/spec/**/*.js', {read: false})
+        .pipe(plumber({
+            errorHandler: function (err) {
+                console.log(err);
+                this.emit('end');
+            }
+        }))  
         .pipe(mocha())
         .once('error', function() { process.exit(1); })
         .once('end', function() { process.exit(); });
