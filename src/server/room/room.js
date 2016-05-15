fb8f048e65cd6a5a1313b33ed04827fb51ee360a
Michael McDermott <mmcdermott@mad-computer-scientist.com>
2016-05-14 21:06:21 -0500

    Moved to using ES6 Class syntax for better reading.

--- src/server/room/room.js
+++ src/server/room/room.js
@@ -1,29 +1,31 @@
 'use strict';
 var util = require('../util/utility-functions');
 
-function Room() {
-    this.id = "";
-    this.name = "";
-    this.seats = 3;
-    this.availableSeats = 3;
+class Room {
+    constructor(id, name, seats, availableSeats) {
+        this.id = "";
+        this.name = "";
+        this.seats = 3;
+        this.availableSeats = 3;
 
-    if (arguments && arguments.length === 1 && !util.isVoid(arguments[0])) {
-        var objectToCopy = arguments[0];
+        if (arguments && arguments.length === 1 && !util.isVoid(arguments[0])) {
+            var objectToCopy = arguments[0];
 
-        this.id = objectToCopy.id;
-        this.name = objectToCopy.name;
-        this.seats = objectToCopy.seats;
-        this.availableSeats = objectToCopy.availableSeats;
+            this.id = objectToCopy.id;
+            this.name = objectToCopy.name;
+            this.seats = objectToCopy.seats;
+            this.availableSeats = objectToCopy.availableSeats;
+        }
+    }
+
+    isValid() {
+        var isValid = true,
+            me = this;
+
+        isValid = isValid && (typeof me.seats === 'number' && me.seats >= 3 && me.seats <= 5);
+
+        return isValid;
     }
 }
-
-Room.prototype.isValid = function() {
-    var isValid = true,
-        me = this;
-
-    isValid = isValid && (typeof me.seats === 'number' && me.seats >= 3 && me.seats <= 5);
-
-    return isValid;
-};
 
 module.exports.Room = Room;
--- src/server/user/user.js
+++ src/server/user/user.js
@@ -1,21 +1,23 @@
 'use strict';
 var util = require('../util/utility-functions');
 
-function User() {
-    this.nickname = "";
-    this.ticket = "";
+class User {
+    constructor(nickname, ticket) {
+        this.nickname = "";
+        this.ticket = "";
 
-    if (arguments && arguments.length === 1 && !util.isVoid(arguments[0])) {
-        var objectToCopy = arguments[0];
+        if (arguments && arguments.length === 1 && !util.isVoid(arguments[0])) {
+            var objectToCopy = arguments[0];
 
-        this.nickname = objectToCopy.nickname || this.nickname;
-        this.ticket = objectToCopy.ticket || this.ticket;
+            this.nickname = objectToCopy.nickname || this.nickname;
+            this.ticket = objectToCopy.ticket || this.ticket;
+        }
+    }
+
+    isValid () {
+        var me = this;
+        return !!(me.nickname && me.nickname.length <= 255);
     }
 }
-
-User.prototype.isValid = function() {
-    var me = this;
-    return !!(me.nickname && me.nickname.length <= 255);
-};
 
 module.exports.User = User;
