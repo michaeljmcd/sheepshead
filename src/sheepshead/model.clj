ec6b20c765d6fc67e74f4730e5eb0afb5a0063c7
Michael McDermott <mcdermott.michaelj@gmail.com>
2017-10-11 09:16:28 -0500

    Tinkering with some game primitives.

---
+++ src/sheepshead/model.clj
@@ -0,0 +1,15 @@
+(ns sheepshead.model)
+
+(defn create-deck []
+  (let [suits [:hearts :clubs :spades :diamonds]
+        ranks (range 7 15)]
+    (flatten (mapv (fn [a] (mapv #(do {:rank a :suit %}) suits)) ranks))))
+
+(defn is-jack? [card] (= (:rank card) 11))
+
+(defn is-queen? [card] (= (:rank card) 12))
+
+(defn is-trump? [card]
+  (or (= (:suit card) :diamonds)
+      (is-jack? card)
+      (is-queen? card)))
